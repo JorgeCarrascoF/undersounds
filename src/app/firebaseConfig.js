cc3ad9eaa35ce770b1c108db79c831d308560740
Jorge Carrasco <105206644+JorgeCarrascoF@users.noreply.github.com>
2025-05-09 18:35:53 +0200

    Update firebaseConfig.js deleting apikey

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -3,15 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDzmNsBMGv0qi8UqUuev4FlnaycU5lj-nk",
-  authDomain: "undersounds-cd613.firebaseapp.com",
-  projectId: "undersounds-cd613",
-  storageBucket: "undersounds-cd613.firebasestorage.app",
-  messagingSenderId: "630912171869",
-  appId: "1:630912171869:web:30440f07d30eff9ae232b4",
-  measurementId: "G-XN673S4VT4"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
